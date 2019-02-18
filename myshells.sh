f324d12a1b42468bf528e9f78714f868a362295b
NeupOrng <neuporng18@kit.edu.kh>
2019-02-18 16:00:33 +0700

    testing

---
+++ myshells.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/env bash
+# my first script
+#echo "hello world" # hello world
+
+#echo $BASH
+#echo $BASH_VERSION
+#echo $HOME
+#echo $PWD
+
+#name=Orng# variable name should not start with number
+#echo The name is $name
+
+echo "Enter name : "
+read name
+echo "Entered name : $name"
